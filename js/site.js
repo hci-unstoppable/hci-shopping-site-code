9f34e02f39e7bd9263f801d3015f71b104542354
Jakub Urbaniak <jurbaniak@hawk.iit.edu>
2022-03-18 17:12:55 -0700

    Add js for all fieldsets

--- js/site.js
+++ js/site.js
@@ -6,3 +6,27 @@ if (creditCardPaymentFieldset !== null) {
   creditCardPaymentFieldset.setAttribute('disabled', 'disabled');
   creditCardPaymentFieldset.setAttribute('aria-hidden', 'true');
 }
+
+var billingFieldset = document.querySelector('fieldset[name="billing-payment"]');
+
+if (billingFieldset !== null) {
+
+  billingFieldset.setAttribute('disabled', 'disabled');
+  billingFieldset.setAttribute('aria-hidden', 'true');
+}
+
+var billingLocationFieldset = document.querySelector('fieldset[name="shipping-location"]');
+
+if (billingLocationFieldset !== null) {
+
+  billingLocationFieldset.setAttribute('disabled', 'disabled');
+  billingLocationFieldset.setAttribute('aria-hidden', 'true');
+}
+
+var personalInformationFieldset = document.querySelector('fieldset[name="personal-information-field"]');
+
+if (personalInformationFieldset !== null) {
+
+  personalInformationFieldset.setAttribute('disabled', 'disabled');
+  personalInformationFieldset.setAttribute('aria-hidden', 'true');
+}
