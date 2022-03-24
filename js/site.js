f8d86a2eb580846a7547b476401444aadc61f628
Jakub Urbaniak <jurbaniak@hawk.iit.edu>
2022-03-24 13:02:17 -0700

    Fix js routing between pages; Add js for selector

--- js/site.js
+++ js/site.js
@@ -1,1 +1,12 @@
-
+const validate = function(ev){
+  if (html.id === "shipping-information-page") {
+      let failure = [];
+      //for select statements
+      let select = document.getElementById('shipping-options');
+      //if it's the first option, output an error
+      if (select.selectedIndex === 0) {
+        failure.push({input:'shipping-options', msg:'Three Options Available'})
+      }
+    return failure;
+  }
+}
